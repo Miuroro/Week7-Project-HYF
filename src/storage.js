11626380f8dce033bec35faa9a3a2455ffababb0
dagimgit config --global user.email dagimvanhaileselassie@gmail.com <hackyourfuture@HackYourFutures-MacBook-Pro.local>
2026-02-20 21:37:15 +0100

    fix(storage): correct saveCourseData and saveTraineeData to accept data parameter

--- src/storage.js
+++ src/storage.js
@@ -4,17 +4,23 @@ const TRAINEE_DATA_FILE_PATH = './data/trainees.json';
 const COURSE_DATA_FILE_PATH = './data/courses.json';
 
 export function loadTraineeData() {
-  // Use the fs module to read the trainees.json file and return the data as a JavaScript object  
+  // Use the fs module to read the trainees.json file and return the data as a JavaScript object
+  const load = fs.readFileSync(TRAINEE_DATA_FILE_PATH, 'utf8');
+  return JSON.parse(load);
 }
 
-export function saveTraineeData() {
-  // Use the fs module to write the updated trainee data back to the trainees.json file 
+export function saveTraineeData(data) {
+  // Use the fs module to write the updated trainee data back to the trainees.json file
+  fs.writeFileSync(TRAINEE_DATA_FILE_PATH, JSON.stringify(data, null, 2));
 }
 
 export function loadCourseData() {
   // TODO: Implement
+  const load = fs.readFileSync(COURSE_DATA_FILE_PATH, 'utf8');
+  return JSON.parse(load);
 }
 
-export function saveCourseData() {
+export function saveCourseData(data) {
   // TODO: Implement
+  fs.writeFileSync(COURSE_DATA_FILE_PATH, JSON.stringify(data, null, 2));
 }
