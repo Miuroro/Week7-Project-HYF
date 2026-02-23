2d897588957ca5cbc7d0641918dc273b2e25957e
Dagim02 <dagimvanhaileselassie@gmail.com>
2026-02-23 13:48:47 +0100

    new update for parser using node:url

--- src/storage.js
+++ src/storage.js
@@ -1,7 +1,12 @@
 import fs from 'node:fs';
+import { fileURLToPath } from 'node:url';
 
-const TRAINEE_DATA_FILE_PATH = './data/trainees.json';
-const COURSE_DATA_FILE_PATH = './data/courses.json';
+const TRAINEE_DATA_FILE_PATH = fileURLToPath(
+  new URL('../data/trainees.json', import.meta.url)
+);
+const COURSE_DATA_FILE_PATH = fileURLToPath(
+  new URL('../data/courses.json', import.meta.url)
+);
 
 export function loadTraineeData() {
   const load = fs.readFileSync(TRAINEE_DATA_FILE_PATH, 'utf8');
