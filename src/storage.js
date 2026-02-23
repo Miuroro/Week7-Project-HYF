81282b46c44652c098a35a3149f609f339adeb6d
Dagim02 <dagimvanhaileselassie@gmail.com>
2026-02-23 12:23:44 +0100

    updated-storage-logic

--- src/storage.js
+++ src/storage.js
@@ -4,17 +4,18 @@ const TRAINEE_DATA_FILE_PATH = './data/trainees.json';
 const COURSE_DATA_FILE_PATH = './data/courses.json';
 
 export function loadTraineeData() {
-  // Use the fs module to read the trainees.json file and return the data as a JavaScript object  
+  const load = fs.readFileSync(TRAINEE_DATA_FILE_PATH, 'utf-8');
+  return JSON.parse(load);
 }
 
-export function saveTraineeData() {
-  // Use the fs module to write the updated trainee data back to the trainees.json file 
+export function saveTraineeData(data) {
+  fs.writeFileSync(TRAINEE_DATA_FILE_PATH, JSON.stringify(data, null, 2));
 }
 
 export function loadCourseData() {
-  // TODO: Implement
+  const load = fs.readFileSync(COURSE_DATA_FILE_PATH, 'utf8');
+  return JSON.parse(load);
 }
-
-export function saveCourseData() {
-  // TODO: Implement
+export function saveCourseData(data) {
+  fs.writeFileSync(COURSE_DATA_FILE_PATH, JSON.stringify(data, null, 2));
 }
